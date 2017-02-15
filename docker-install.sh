74b67deff488349286523fd77b1f428035660f8d
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2017-02-16 01:16:49 +0300

    fix docker setup

--- docker-install.sh
+++ docker-install.sh
@@ -2,6 +2,8 @@
 
 sudo apt-get update && \
         sudo apt-get -y install docker.io && \
-        sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
+        sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker && \
+        sudo gpasswd -a `whoami` docker && \
+        echo "need restart ssh session"
 
 
