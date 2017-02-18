beb4d55bef84a11466383a1cb83a25330645b5e1
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2017-02-18 13:41:09 +0300

    changed install script

--- docker-install.sh
+++ docker-install.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update && \
     sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && \
-    sudo apt-add-repository "deb https://apt.dockerproject.org/repo ubuntu-$(uname -s)  main" && \
+    sudo apt-add-repository "deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release --codename | cut -f2)  main" && \
     sudo apt-get update && \
     sudo apt-get install -y docker-engine && \
     sudo usermod -aG docker $(whoami)
