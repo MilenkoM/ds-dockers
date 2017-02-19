317975d02d3dcf30c5f5034029cc50ff19c00c61
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2017-02-19 21:45:35 +0300

    docker proper install

--- docker-install.sh
+++ docker-install.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+sudo apt-get install -y --no-install-recommends \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
+
 sudo apt-get update && \
-    sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && \
-    sudo apt-add-repository "deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release --codename | cut -f2)  main" && \
-    sudo apt-get update && \
-    sudo apt-get install -y docker-engine && \
-    sudo usermod -aG docker $(whoami)
+    sudo apt-get -y install docker-engine && \
+    sudo usermod -aG docker $(whoami) && \
+    sudo rm -rf /var/lib/apt/lists/*
