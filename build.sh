4a84c6a3beb5d7ed4c4d3c93640dcd4cba2d5deb
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2017-02-21 16:54:41 +0300

    change directory for build

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ while getopts 'bso' flag; do
        docker build -t ferres/base:cpu -f base/Dockerfile.cpu ./base ;;
     s) docker build -t ferres/sci:gpu -f sci/Dockerfile.gpu ./sci && \
        docker build -t ferres/sci:cpu -f sci/Dockerfile.cpu ./sci ;;
-    o) docker build -t ferres/openai:gpu -f openai/Dockerfile.gpu ./sci && \
-       docker build -t ferres/openai:cpu -f openai/Dockerfile.cpu ./sci ;;
+    o) docker build -t ferres/openai:gpu -f openai/Dockerfile.gpu ./openai && \
+       docker build -t ferres/openai:cpu -f openai/Dockerfile.cpu ./openai ;;
     *) error "Unexpected option ${flag}" ;;
   esac
 done
