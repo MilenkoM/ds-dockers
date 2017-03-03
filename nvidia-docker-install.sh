637154ca747a3102148297b1169bff013d89d5e9
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2017-03-03 19:09:41 +0300

    remove sudo from file

--- nvidia-docker-install.sh
+++ nvidia-docker-install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Install official NVIDIA driver package
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
-sudo apt-get update && sudo apt-get install -y --no-install-recommends cuda-drivers
+apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+apt-get update && apt-get install -y --no-install-recommends cuda-drivers
 
 # Install nvidia-docker and nvidia-docker-plugin
 wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0/nvidia-docker_1.0.0-1_amd64.deb
-sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
+dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
 
 # Clean apt lists
-sudo rm -rf /var/lib/apt/lists/*
+rm -rf /var/lib/apt/lists/*
