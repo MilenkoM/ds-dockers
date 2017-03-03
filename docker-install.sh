8e057593cbbde0977072bb50e2ab5c8b19fb7a64
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2017-03-03 19:08:39 +0300

    remove sudo from file

--- docker-install.sh
+++ docker-install.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y --no-install-recommends \
+apt-get install -y --no-install-recommends \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 
-curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+curl -fsSL https://apt.dockerproject.org/gpg | apt-key add -
 
-sudo add-apt-repository \
+add-apt-repository \
        "deb https://apt.dockerproject.org/repo/ \
        ubuntu-$(lsb_release -cs) \
        main"
 
-sudo apt-get update && \
-    sudo apt-get -y install docker-engine && \
-    sudo usermod -aG docker $(whoami) && \
-    sudo rm -rf /var/lib/apt/lists/*
+apt-get update && \
+    apt-get -y install docker-engine && \
+    usermod -aG docker $(whoami) && \
+    rm -rf /var/lib/apt/lists/*
